cd8c15f61225c331e3c27ef1baef4b02fee3b5c5
rustamov-bite <assasins_murza006@mail.ru>
2021-01-29 22:32:30 +0600

    initial project

---
+++ src/main/java/com/example/labwork6hw/Labwork6hwApplication.java
@@ -0,0 +1,13 @@
+package com.example.labwork6hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labwork6hwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labwork6hwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labwork6hw/Labwork6hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labwork6hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labwork6hwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
