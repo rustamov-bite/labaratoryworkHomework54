c716749e96aefbcfc4d63db12de66ab138febdf1
rustamov-bite <assasins_murza006@mail.ru>
2021-01-31 20:46:08 +0600

    updated repositories updated application.properties

--- src/main/java/com/example/labwork6hw/repositories/EventRepo.java
+++ src/main/java/com/example/labwork6hw/repositories/EventRepo.java
@@ -3,6 +3,10 @@ package com.example.labwork6hw.repositories;
 import com.example.labwork6hw.entities.Event;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EventRepo extends CrudRepository<Event, String> {
     Event findEventById(String id);
+    List<Event> findAll();
+    boolean existsEventById(String id);
 }
--- src/main/java/com/example/labwork6hw/repositories/SubscriptionRepo.java
+++ src/main/java/com/example/labwork6hw/repositories/SubscriptionRepo.java
@@ -1,8 +1,15 @@
 package com.example.labwork6hw.repositories;
 
+import com.example.labwork6hw.entities.Event;
 import com.example.labwork6hw.entities.Subscription;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SubscriptionRepo extends CrudRepository<Subscription, String> {
-    Subscription findSubscriptionById(String id);
+    List<Subscription> findSubscriptionsByEmail(String email);
+    List<Subscription> findAll();
+    boolean existsSubscriptionByEvent(Event event);
+    boolean existsSubscriptionByIdAndEmail(String id, String email);
+    void deleteSubscriptionByIdAndEmail(String id, String email);
 }
